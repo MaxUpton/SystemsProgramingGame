8c305ac70ee390393907e3d66301f9fda6da661d
moosmanjr@gmail.com <>
2026-04-23 15:45:08 -0700

    added main menu updated the game and scene manager so we now have a main menu to use with the project

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,20 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void startGame()
+    {
+        SceneManager.LoadScene("360Project");
+    }
+
+    // Update is called once per frame
+    public void endGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
